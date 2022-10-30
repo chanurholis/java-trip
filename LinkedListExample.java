88a5a83009fcb35c2a5bb1c7c05f1fc0fccfd3a6
Chacha Nurholis <chachanurholis29@gmail.com>
2022-10-30 17:51:02 +0700

    feat: add linked list example - error

---
+++ LinkedListExample.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+public class LinkedListExample {
+  public static void main(String[] args) {
+    LinkedList cars = new LinkedList();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
+    System.out.println(cars);
+  }
+}
